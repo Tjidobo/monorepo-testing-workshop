6f469be31ab00c54de468bf57e917236bd4c9625
Tjitte Bouma <tjitte.bouma@winvision.nl>
2023-09-21 20:38:10 +0200

    button testid parameterized

--- apps/test-automation/src/app/contact/page.tsx
+++ apps/test-automation/src/app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Heading title="Contact Us" />
-      <Button title="Click me" />
+      <Button title="Click me" testid="contact-button"/>
     </main>
   );
 }
--- packages/ui/src/atoms/button.tsx
+++ packages/ui/src/atoms/button.tsx
@@ -1,8 +1,8 @@
-export function Button({ title }: { title: string }): JSX.Element {
+export function Button({ title, testid }: { title: string, testid: string }): JSX.Element {
   return (
       <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      data-testid="contact-button"
+      data-testid={testid}
       type="button"
     >
       {title}
